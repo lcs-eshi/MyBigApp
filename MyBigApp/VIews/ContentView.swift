bf43315bb1ae754e06b92b53f677d4d17e02e6b3
Ethan Shi <eshi@lcs.on.ca>
2026-03-02 12:32:09 -0500

    Class finished.

---
+++ MyBigApp/VIews/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  MyBigApp
+//
+//  Created by Yicheng Shi on 2026-03-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Rectangle()
+                .frame(height:100)
+            HStack {
+                
+                Text("Today Wednesday Dec 17")
+                Spacer()
+                VStack {
+                    Circle()
+                    Rectangle()
+                        .frame(width:150)
+                }
+            }
+            Rectangle()
+            HStack {
+                Rectangle()
+                    .frame(width: 200)
+                Rectangle()
+            }
+            HStack{
+                Text("Start Planning")
+                
+                Rectangle()
+                    .frame(height: 100)
+                
+                
+                    .padding()
+            }
+        }
+        
+        #Preview {
+            ContentView()
+        }
+        
+    }
+}
---
+++ MyBigApp/VIews/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  MyBigApp
+//
+//  Created by Yicheng Shi on 2026-03-02.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
